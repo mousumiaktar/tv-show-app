7d62daf9bad324ee366647c906069d453d4c820d
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-04-01 12:10:41 +0600

    summery data show

--- src/pages/ShowDetails.js
+++ src/pages/ShowDetails.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
-const ShowDetails = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+function ShowDetails() {
+  const { id } = useParams();
+  const [show, setShow] = useState(null);
+
+  useEffect(() => {
+    const fetchShow = async () => {
+      const response = await fetch(`https://api.tvmaze.com/shows/${id}`);
+      const data = await response.json();
+      console.log(data);
+    };
+    fetchShow();
+  }, [id]);
+
+  
+
+  return (
+    <div>
+      
+    </div>
+  );
+}
 
 export default ShowDetails;
