888d0aa432c67a83056b25186778f5c7d5017c4d
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-04-01 11:50:22 +0600

    img and language show

--- src/pages/AllShows.js
+++ src/pages/AllShows.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const AllShows = ({show}) => {
-    const{name} = show.show
+    const{image, name, language, id} = show.show
     return (
         <div>
+            <img src={image?.medium} alt="" />
             <h1>{name}</h1>
+            <h3>{language}</h3>
         </div>
     );
 };
