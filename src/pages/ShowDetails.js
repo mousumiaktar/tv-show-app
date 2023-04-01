2a935074379b8c078b5cc60d422983a8e0e2b9ae
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-04-01 12:18:22 +0600

    summery

--- src/pages/ShowDetails.js
+++ src/pages/ShowDetails.js
@@ -9,16 +9,20 @@ function ShowDetails() {
     const fetchShow = async () => {
       const response = await fetch(`https://api.tvmaze.com/shows/${id}`);
       const data = await response.json();
-      console.log(data);
+      setShow(data);
     };
     fetchShow();
   }, [id]);
 
-  
+  if (!show) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
-      
+        <img src={show.image?.medium} alt="" />
+      <h1>{show.name}</h1>
+      <p>{show.summary}</p>
     </div>
   );
 }
